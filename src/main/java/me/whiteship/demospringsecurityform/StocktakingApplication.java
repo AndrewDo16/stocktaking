652b181fd85a5c56fa6a752be3716274aa396848
Andrey <danilin1av@yandex.ru>
2023-12-19 21:10:12 +0300

    refactroring base

--- src/main/java/me/whiteship/demospringsecurityform/StocktakingApplication.java
+++ src/main/java/me/whiteship/demospringsecurityform/StocktakingApplication.java
@@ -1,19 +1,15 @@
 package me.whiteship.demospringsecurityform;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableAsync
-public class DemoSpringSecurityFormApplication {
+public class StocktakingApplication {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -21,7 +17,7 @@ public class DemoSpringSecurityFormApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringSecurityFormApplication.class, args);
+		SpringApplication.run(StocktakingApplication.class, args);
 	}
 
 }
